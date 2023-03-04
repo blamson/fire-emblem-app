514191ae77e3df4ceb678a498adf0e26812c0c7b
Brady Lamson <lamsonbrady@gmail.com>
2023-03-03 22:31:26 -0700

    basic engage character growth + class growth tables made

--- app.R
+++ app.R
@@ -18,35 +18,87 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("Fire Emblem Character Growths"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("game",
-                        "Select Game",
-                        c("Engage" = "engage",
-                          "Gaiden" = "gaiden",
-                          "Shadow Dragon" = "shadow-dragon-and-blade-of-light",
-                          "Sacred Stones" = "the-sacred-stones"
-                          ))
+    
+    fluidRow(
+        column(6,
+               sidebarPanel(
+                   selectInput(
+                       "game",
+                       "Select Game",
+                       c("Engage" = "engage",
+                         "Gaiden" = "gaiden",
+                         "Shadow Dragon" = "shadow-dragon-and-blade-of-light",
+                         "Sacred Stones" = "the-sacred-stones"
+                       )
+                   )
+               ),
+               uiOutput("char_select")
+        )
+    ),
+    
+    hr(),
+    
+    fluidRow(
+        column(6,
+            tabsetPanel(
+                tabPanel("Character Growths", dataTableOutput('char_df')),
+                tabPanel("Class Growths", dataTableOutput('class_df'))
+            )       
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            dataTableOutput('df')
+        column(6,
+            dataTableOutput('char_class_df')
         )
     )
+
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    make_df <- reactive({
+    
+    #This creates the character growth df
+    char_df <- reactive({
         readr::read_csv(file = paste("data/", input$game, "/char_growths.csv", sep = "")) %>%
             as_tibble()
     }) 
     
-    output$df <- renderDataTable(make_df())
+    output$char_df <- renderDataTable(char_df(), options = list(pageLength=15))
+    
+    # This creates the class growth df
+    class_df <- reactive({
+        readr::read_csv(file = paste("data/", input$game, "/class_growths.csv", sep = "")) %>%
+            as_tibble()
+    }) 
+    
+    output$class_df <- renderDataTable(class_df(), options = list(pageLength=15))
+    
+    # This generates the list of characters that can be selected
+    output$char_select <- renderUI({
+        selectInput("char_name", "Select a character", choices = sort(unique(char_df()$Name)))
+    })
+    
+    # This df is a specific characters growths added w/ class growths
+    char_class_df <- reactive({
+        char_row <- char_df() %>%
+            filter(Name == input$char_name)
+        
+        df <- rbind(char_row, class_df()) %>%
+            mutate(
+                across(
+                    # ignoring the first column, add the values of the first row to every row. 
+                    -1, ~ . + .[1]
+                )
+            )
+        
+        # First row gets doubled due to above so we divide it by two here.
+        df[1,-1] <- df[1,-1] / 2
+        
+        df
+    })
+    
+    output$char_class_df <- renderDataTable(char_class_df(), options = list(pageLength=15))
+    
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
--- serenes_table_downloader.R
+++ serenes_table_downloader.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(rvest)
+library(tidyr)
 
 serenes_to_df <- function(game = "engage", char_or_class = "characters") {
     link <- paste("https://serenesforest.net/", game, "/", char_or_class, "/", "growth-rates", sep = "")
@@ -10,18 +11,21 @@ serenes_to_df <- function(game = "engage", char_or_class = "characters") {
         html_nodes("table") %>%
         html_table()
 
-    print(df)
+    #print(df)
     
     df <-
         df[[1]] %>%
         mutate(
             across(!Name, as.numeric)
-        ) 
+        ) %>%
+        replace(is.na(.), 0) %>%
+        filter(Name != 'Name')
 
     return(df)
 }
 
-games <- c("shadow-dragon-and-blade-of-light", "gaiden", "engage", "the-sacred-stones")
+#games <- c("shadow-dragon-and-blade-of-light", "gaiden", "engage", "the-sacred-stones")
+games <- c("engage")
 
 for (game in games) {
     df <- serenes_to_df(game = game)
@@ -31,4 +35,14 @@ for (game in games) {
     }
     write.csv(df, file = paste(path, "char_growths.csv", sep=""), row.names = FALSE)
 }
+
+for (game in games) {
+    df <- serenes_to_df(game = game, char_or_class = "classes")
+    path <- paste("data/", game, "/", sep="")
+    if (!file.exists(path)){
+        dir.create(path)
+    }
+    write.csv(df, file = paste(path, "class_growths.csv", sep=""), row.names = FALSE)
+}
+
 # write.csv(engage_character_df, file = "data/engage/char_growths.csv", row.names = FALSE)
