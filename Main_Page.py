ef8fb3d3f1d2de6c73d075dea3829eb3834f3330
Brady Lamson <brady@fingoal.com>
2024-05-31 16:16:54 -0600

    testing something

--- Main_Page.py
+++ Main_Page.py
@@ -4,7 +4,7 @@ st.set_page_config(
     page_title="Fire Emblem Dashboard",
     page_icon="🗡️"
 )
-st.image("images/fire_emblem_logo.jpeg")
+# st.image("images/fire_emblem_logo.jpeg")
 st.sidebar.success("Select a Page above!")
 # with open("README.md") as f:
 #     main_blurb = f.read()
